67b06b1ff61120652a9f970f546dd34ec767f46b
ymukesh012 <ymukesh012@gmail.com>
2024-06-11 06:23:55 +0545

    added functionality

--- LotteryGame/src/Lottery.jsx
+++ LotteryGame/src/Lottery.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
-import { genTicket } from "./helper";
+import { genTicket,sum } from "./helper";
 import "./Lottery.css";
 
 export default function Lottery(){
     let [ticket,SetTicket]=useState(genTicket(3));
+    let isWinning = sum(ticket) === 15;
+    
+    let buyTicket = () => {
+        SetTicket(genTicket(3));
+    };
+
     return(
         <div>
-        <h1>Lottery Game!</h1>
+        <h1 style={{color:"Brown"}}>Angel Lottery Game!</h1>
         <div className="ticket">
             <span>{ticket[0]}</span>
             <span>{ticket[1]}</span>
             <span>{ticket[2]}</span>
         </div>
+        <br></br>
+        <button onClick={buyTicket}>Buy New Ticket</button>
+        <br></br>
+        <h1 style={{color:"green"}}>{isWinning && "Congratulation,App Ko Milega ek Dairy Milk!"}</h1>
         </div>
     );
 }
--- LotteryGame/src/helper.js
+++ LotteryGame/src/helper.js
@@ -3,7 +3,14 @@ function genTicket(n){
     for(let i=0;i<n;i++){
         arr[i]=Math.floor(Math.random()*10);
     }
-    return arr;
-}
+        return arr;
+    
+    }
+    
+  function sum(arr){
+    return arr.reduce((sum,curr)=> sum+curr,0);
+  
+    }
 
-export{genTicket};
+
+export{genTicket, sum };
